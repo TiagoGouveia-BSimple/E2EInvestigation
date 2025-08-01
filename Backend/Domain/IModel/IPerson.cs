9cf5cb307bf382039890b719b935c0edf64e2709
TiagoGouveia-BSimple <tiago.gouveia@b-simple.pt>
2025-08-01 11:00:51 +0100

    let there be light

---
+++ Backend/Domain/IModel/IPerson.cs
@@ -0,0 +1,8 @@
+namespace Backend.Domain.IModel;
+
+public interface IPerson
+{
+    string Name { get; }
+    string Surname { get; }
+    int Age { get; }
+}
---
+++ Backend/Domain/Model/Person.cs
@@ -0,0 +1,19 @@
+using Backend.Domain.IModel;
+
+namespace Backend.Domain.Model;
+
+class Person : IPerson
+{
+    public string Name { get; private set; }
+
+    public string Surname { get; private set; }
+
+    public int Age { get; private set; }
+
+    public Person(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+}
