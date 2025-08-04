386ce4b775da35ee5d319749940bbf67b0d968b5
Bruno <bruno.lopes@b-simple.pt>
2025-08-04 12:41:26 +0100

    new Fronted

---
+++ Frontend/src/app/person-list/person-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-list',
+  imports: [],
+  templateUrl: './person-list.component.html',
+  styleUrl: './person-list.component.css'
+})
+export class PersonListComponent {
+
+}
